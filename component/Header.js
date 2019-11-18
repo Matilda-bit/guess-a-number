a098631e5dba24ca5d4978c12ab00f465eaf7eac
Matilda-bit <polinaovras@gmail.com>
2019-11-18 22:18:18 +0200

    title top

--- component/Header.js
+++ component/Header.js
@@ -4,17 +4,26 @@ import {View, Text, StyleSheet} from 'react-native';
 
 const Header = props => {
     return (
-        <View style={styles.headerTitle}>
-            <Text>{props.title}</Text>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
 
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    headerTitle : {
-        width: '90%',
+    header: {
+        width: '100%',
         height: 90,
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    headerTitle: {
+        color: 'black',
+        fontSize: 18
     }
 });
 
