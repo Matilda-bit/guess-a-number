765d8ed4ab09d019230ceba0f57a98f90b25e327
Matilda-bit <polinaovras@gmail.com>
2019-11-19 20:47:35 +0200

    create game screen

---
+++ screens/GameScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet, ToucheAble} from 'react-native';
+
+const GameScreen = props => {
+    return(
+        <View></View>
+    );
+};
+
+const styles = StyleSheet.create({
+    game: {}
+});
+
+export default GameScreen;
