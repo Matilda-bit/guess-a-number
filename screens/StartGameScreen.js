f64e129071ac4bed5bf1dee65ce4c93c8177b1c7
Matilda-bit <polinaovras@gmail.com>
2019-11-19 16:12:35 +0200

    style Input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import Card from '../components/Card';
+import Input from '../components/Input';
 import Colors from '../constants/colors';
+
 
 const StartGameScreen = props => {
     return (
@@ -9,7 +11,7 @@ const StartGameScreen = props => {
             <Text style={styles.title}>Start New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}><Button title="Reset"  onPress={() => {}} color={Colors.accent} /></View> 
                     <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View>
