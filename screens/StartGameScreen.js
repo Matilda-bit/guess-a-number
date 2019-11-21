1d2718fb9e7eb9dd7de1eb51d901517b2235df18
Matilda-bit <polinaovras@gmail.com>
2019-11-21 14:42:01 +0200

    add a Main Button configure

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,6 +11,8 @@ import {
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/colors';
+
+import MainButton from '../components/MainButton';
 import NumberContainer from '../components/NumberContainer';
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
@@ -54,10 +56,8 @@ const StartGameScreen = props => {
         <Card style={styles.summaryContainer}>
             <Text>You Selected:</Text>
             <NumberContainer>{selectedNumber}</NumberContainer>
-            <Button 
-                title="START GAME"
-                onPress={() => props.onStartGame(selectedNumber)}
-            />
+            <MainButton 
+                onPress={() => props.onStartGame(selectedNumber)}>START GAME</MainButton>
         </Card>
         );
     }
